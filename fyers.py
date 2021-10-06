dc7294e972d5bc6ad21eaec17b411c7e8ea8a745
kenil2 <91467506+kenil2@users.noreply.github.com>
2021-10-06 11:45:06 +0530

    Update fyers.py

--- fyers.py
+++ fyers.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 from fyers_api import fyersModel
 from fyers_api import accessToken
+
 chrome_options = Options()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
@@ -54,10 +55,8 @@ def get_token():
 access_token = get_token()
 fyers = fyersModel.FyersModel(client_id=app_id, token=access_token)
 
-
-
 while True:
-    
+
     @bot.message_handler(commands='pnl')
     def get_pnl(msg1):
         positions = fyers.positions()
@@ -67,30 +66,27 @@ while True:
         bot.reply_to(msg1, positions_data)
 
 
-
-    def r1(stocks):
+    def r1():
         return "abc"
+
 
     @bot.message_handler(func=r1)
     def r2(stocks):
         x = stocks.text
         x1 = x.replace("/", "")
+        x2 = x1[:1]
+        x3 = x1[:4]
+        x4 = x1[1:]
         chat_id = stocks.chat.id
-        print(chat_id)
-
-
-        try:
-
-            data1 = {"symbols": "NSE:" + x1 + "-EQ"}
+        if x2 == "p":
+            data1 = {"symbols": "NSE:" + x1[1:] + "-EQ"}
             quote = fyers.quotes(data1)
-            cmp = float
-            ch = float
             cmp = quote['d'][0]['v']['lp']
             ch = quote['d'][0]['v']['ch']
             chp = quote['d'][0]['v']['chp']
             bid = quote['d'][0]['v']['bid']
             ask = quote['d'][0]['v']['ask']
-            open = quote['d'][0]['v']['open_price']
+            open1 = quote['d'][0]['v']['open_price']
             previos_close = quote['d'][0]['v']['prev_close_price']
             low = quote['d'][0]['v']['low_price']
             high = quote['d'][0]['v']['high_price']
@@ -99,72 +95,57 @@ while True:
                 '{:n}'.format(ch)) + ")" + "   " + "(" + str('{:n}'.format(chp)) + "%)""\nBid : " + str(
                 '{:n}'.format(bid)) + "   Ask : " + str('{:n}'.format(ask)) + "\nHigh : " + str(
                 '{:n}'.format(high)) + "   Low : " + str('{:n}'.format(low)) + "\nOpen : " + str(
-                '{:n}'.format(open)) + "   Previos Close : " + str('{:n}'.format(previos_close)) + "\nVolume : " + str(
+                '{:n}'.format(open1)) + "   Previos Close : " + str('{:n}'.format(previos_close)) + "\nVolume : " + str(
                 '{:n}'.format(volume)))
+        else:
+            bot.reply_to(stocks, "please enter symbol in valid format")
 
-        except:
+        if x3 == "o":
+            name = x1[4:]
+            list1 = name.splitlines()
+            symbol = list1[0]
+            user_qty = list1[1]
+            qty = abs(int(list1[1]))
+            if int(user_qty) < 0:
+                side = -1
+            else:
+                side = 1
+            side = side
+            limitprice = float(list1[2])
+            offlineorder = list1[3]
 
-            try:
-                name = x1
+            data = dict(symbol="NSE:" + symbol + "-EQ", qty=qty, type=1, side=side, productType="CNC",
+                        limitPrice=limitprice,
+                        stopPrice=0, validity="DAY", disclosedQty=0, offlineOrder=offlineorder, stopLoss=0,
+                        takeProfit=0)
 
-                list = name.splitlines()
-                symbol = list[0]
-                user_qty = list[1]
-                qty = abs(int(list[1]))
-                if int(user_qty)<0:
-                    side = -1
-                else:
-                    side = 1
-                side = side
-                limitprice = float(list[2])
-                offlineorder = list[3]
+            response = fyers.place_order(data)
+            order_status = response['message']
+            bot.reply_to(stocks, order_status)
 
+        else:
+            bot.reply_to(stocks, "please enter order form in valid format")
 
-                data = dict(symbol="NSE:"+symbol+"-EQ", qty=qty, type=1, side=side, productType="CNC", limitPrice=limitprice,
-                        stopPrice=0, validity="DAY", disclosedQty=0, offlineOrder=offlineorder, stopLoss=0, takeProfit=0)
+        if x4 == "c":
+            url1 = "https://www.google.com/finance/quote/" + x1[1:] + ":NSE"
+            url2 = "https://www.web2pdfconvert.com/to/img"
+            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),
+                                      chrome_options=chrome_options)
+            driver.get(url2)
+            WebDriverWait(driver, 10).until(
+                ec.visibility_of_element_located((By.XPATH, '//input[@class="js-url-input"]')))
+            driver.find_element_by_xpath("//input[@class='js-url-input']").click()
+            driver.find_element_by_xpath("//input[@class='js-url-input']").send_keys(url1)
+            driver.find_element_by_xpath("//div[@class='convert-icon cursor-pointer js-convert-btn']").click()
+            sleep(20)
+            pic_url = driver.find_element_by_xpath(
+                "//a[@class='btn btn-large btn-primary mt-2 pt-2 js-download-btn']").get_attribute("href")
+            driver.close()
 
-                response = fyers.place_order(data)
-                order_status = response['message']
-                bot.reply_to(stocks, order_status)
+            bot.send_photo(photo=pic_url, chat_id=chat_id)
 
-            except:
-
-                try:
-                    x3 = x1[1:]
-                    print(x3)
-
-                    url1 = "https://www.google.com/finance/quote/"+x3+":NSE"
-                    url2 = "https://www.web2pdfconvert.com/to/img"
-                    print(url1)
-                    
-
-                    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-                    driver.get(url2)
-                    print('f')
-                    driver.get("https://www.web2pdfconvert.com/to/img")
-                    sleep(5)
-                    print('a')
-                    driver.find_element_by_xpath("//input[@class='js-url-input']").click()
-                    print('b')
-                    driver.find_element_by_xpath("//input[@class='js-url-input']").send_keys(url1)
-                    print('c')
-                    driver.find_element_by_xpath("//div[@class='convert-icon cursor-pointer js-convert-btn']").click()
-                    print('d')
-                    sleep(15)  
-                    
-                    pic_url = driver.find_element_by_xpath(
-                        "//a[@class='btn btn-large btn-primary mt-2 pt-2 js-download-btn']").get_attribute("href")
-                    driver.close()
-                    print(pic_url)
-
-
-                    bot.send_photo(photo=pic_url, chat_id=chat_id)
-
-                except:
-
-                    bot.reply_to(stocks, "please enter in format")
-
+        else:
+            bot.reply_to(stocks, "please enter symbol in valid format")
 
 
     bot.polling()
-    sleep(20)
